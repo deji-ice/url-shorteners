d3890981bda1967b587becc903e5147c970825d2
Aguda <agudaoluwanifemi@gmail.com>
2022-12-04 22:28:48 +0100

    installed tailwind

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
+      {/* <header className="App-header">
+      </header> */}
+      <h1 className='text-red-600'>Hello world</h1>
     </div>
   );
 }
