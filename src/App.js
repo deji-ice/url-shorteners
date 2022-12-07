0a7494b88f43161c5fe06fa08da07ad2911a86e7
Aguda <agudaoluwanifemi@gmail.com>
2022-12-07 15:25:01 +0100

    created the hero section
    added the illustration to depict working
    
    added hover functionality to the get started button

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import NavBar from './Components/Header';
+import HeroSection from './Components/HeroSection';
 function App() {
   return (
 <>
 <div >
   <NavBar/>
+  <HeroSection />
 </div>
 </>
   );
